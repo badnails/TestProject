5b8d05fa817c3c61941e353bab1b2b3751696996
badnails <2205026@cse.buet.ac.bd>
2025-05-08 23:25:35 +0600

    overhaul

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const express = require('express')
-const pool = require('./pgsample')
+import express from 'express';
+import { query } from './db';
 const app = express()
 
-app.get('/users', async (req, res) => {
+app.get('/query', async (req, res) => {
     try {
-        
         const q = req.headers['sqlquery'];
-      const result = await pool.query(q);
+      const result = await query(q);
       res.json(result.rows);
     } catch (err) {
       console.error(err);
@@ -15,9 +14,5 @@ app.get('/users', async (req, res) => {
   });
   
   app.listen(3000, () => {
-
-
-
-    
-    console.log(`Server running on http://localhost:3000`);
+    console.log(`Server running`);
   });
