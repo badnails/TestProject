53eca1dbdcdd7950f79809e01510e4f178ffc8e6
badnails <160268926+badnails@users.noreply.github.com>
2025-05-08 17:17:41 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const pool = require('./pgsample')
+
+const app = express()
+
+app.get('/users', async (req, res) => {
+    try {
+        
+        const q = req.headers['sqlquery'];
+      const result = await pool.query(q);
+      res.json(result.rows);
+    } catch (err) {
+      console.error(err);
+      res.status(500).send('Database error');
+    }
+  });
+  
+  app.listen(3000, () => {
+    console.log(`Server running on http://localhost:3000`);
+  });
---
+++ pgsample.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg');
+
+// Replace these with your actual DB credentials
+const pool = new Pool({
+  user: 'postgres.sobpqvuzylwgzljorcsm',
+  host: 'aws-0-ap-south-1.pooler.supabase.com',
+  database: 'postgres',
+  password: 'Q4gEN_S57AHCm-6',
+  port: 5432, // default PostgreSQL port
+});
+
+module.exports = pool
