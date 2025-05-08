7ba67d078b6d3162d4b84af6cece48a87762f127
badnails <2205026@cse.buet.ac.bd>
2025-05-08 23:48:57 +0600

    overhaul fix

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
-import express from 'express';
-import { query } from './db';
-const app = express()
+import express from "express";
+import pool from "./db.js";
+const app = express();
 
-app.get('/query', async (req, res) => {
-    try {
-        const q = req.headers['sqlquery'];
-      const result = await query(q);
-      res.json(result.rows);
-    } catch (err) {
-      console.error(err);
-      res.status(500).send('Database error');
-    }
-  });
-  
-  app.listen(3000, () => {
-    console.log(`Server running`);
-  });
+app.get("/query", async (req, res) => {
+  try {
+    const q = req.headers["sqlquery"];
+    const result = await pool.query(q);
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Database error");
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server running`);
+});
