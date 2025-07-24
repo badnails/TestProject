8b42cd4437f93a03918d11196b4e58f566fd136d
Safayat03Kabir <vercingetorixvercingetorix01@gmail.com>
2025-07-24 20:35:50 +0600

    CORS

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import gatewayRouter from "./routes/gateway.js";
 import {InfoGetter} from "./middleware/getter.js";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
